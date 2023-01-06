ab6232d6a7327d7b05f6df1ab9aa12f386259b31
Sarsen Whatmore <Rhinoam@gmail.com>
2023-01-05 19:06:09 -0500

    grabs question

--- scripts/content.js
+++ scripts/content.js
@@ -14,14 +14,24 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
     let domain = (new URL(url)).hostname.split('.')[0]
     console.log(domain)    
 
+    const info = document.getElementsByClassName('_1l1MA')[0].children
+    if(info != null) {
+        console.log(info)
+        let question = info[0].innerText + " " +  info[1].innerText
+        console.log(question)
+    }
+
+    let language = document.getElementsByClassName("text-xs text-label-2 dark:text-dark-label-2")
+
+
     const page = document.querySelector('body');
     // console.log("test ", page)
     // if(page) page.style.backgroundColor = "red";
 
     // Some styles for a fancy sidebar
-    iframe.style.cssText = 'position:fixed;top:0;right:0;display:block;' +
-                           'width:500px;height:100%;z-index:1000;';
-    document.body.appendChild(iframe);
+    // iframe.style.cssText = 'position:fixed;top:0;right:0;display:block;' +
+    //                        'width:500px;height:100%;z-index:1000;';
+    // document.body.appendChild(iframe);
 }
 
 
